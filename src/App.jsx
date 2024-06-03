16f6b383bd7aacfdfdb695b2508ecdaca57460d0
Sara Chang <sara@Avocado.lan>
2024-06-03 16:08:12 -0700

    build Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.scss";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <>
+      <Button label={"label"} />
       <h1>Penguins</h1>
       <h2>Penguins</h2>
       <h3>Penguins</h3>
