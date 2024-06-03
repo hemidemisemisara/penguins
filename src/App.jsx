24c8c78c32e22959ea71e8f1ac5faf1525d3ccdc
Sara Chang <sara@Avocado.lan>
2024-06-03 16:37:42 -0700

    build SubHeading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.scss";
 import Button from "./components/Button/Button";
 import Heading from "./components/Heading/Heading";
+import SubHeading from "./components/SubHeading/SubHeading";
 
 function App() {
   return (
     <>
       <Button label={"label"} />
       <Heading heading={"shared memories"} />
+      <SubHeading text={"friends since Apr 2023"} />
+      <SubHeading text={"from Sara"} />
       <h1>Penguins</h1>
       <h2>Penguins</h2>
       <h3>Penguins</h3>
---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import "./SubHeading.scss";
+
+export default function SubHeading({ text }) {
+  return (
+    <div className="sub-heading">
+      <div className="sub-heading__line"></div>
+      <p className="sub-heading__text">{text}</p>
+      <div className="sub-heading__line"></div>
+    </div>
+  );
+}
+
+SubHeading.propTypes = {
+  text: PropTypes.string.isRequired,
+};
