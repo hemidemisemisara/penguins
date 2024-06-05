4c812d85fe2aad0e4a61a6dcf190fea166885e24
Sara Chang <sara@Avocado.lan>
2024-06-04 23:21:13 -0700

    render first impression section with fetched data

--- src/components/SectionFirstImpressions/SectionFirstImpressions.jsx
+++ src/components/SectionFirstImpressions/SectionFirstImpressions.jsx
@@ -7,27 +7,24 @@ import SubHeading from "../SubHeading/SubHeading";
 export default function SectionFirstImpressions({ friendshipDetails }) {
   const firstImpressions = friendshipDetails["first-impressions"];
   console.log("firstImpressions", firstImpressions);
+  firstImpressions.map((impression) => console.log(impression));
   return (
     <section className="first-impressions">
       <Heading heading="first impressions" />
-      {firstImpressions.map((firstImpression) => {
-        <>
-          <h1>hello</h1>
-          <div className="first-impressions__user">
-            <SubHeading text={firstImpression["created-by"]} />
+      {firstImpressions.map((impression) => {
+        return (
+          <div key={impression.id} className="first-impressions__user">
+            <SubHeading text={`from ${impression["created-by"]}`} />
             <PhotoCardSmall
-              source={firstImpression.image}
-              alt="first impression image"
-              title={firstImpression.image}
+              source={impression.image}
+              alt={impression["image-title"]}
+              title={impression["image-title"]}
             />
-            <p className="first-impressions__text">
-              {firstImpression.description}
-            </p>
-            <Button label="edit" />
+            <p className="first-impressions__text">{impression.description}</p>
           </div>
-          ;
-        </>;
+        );
       })}
+      <Button label="edit" />
     </section>
   );
 }
