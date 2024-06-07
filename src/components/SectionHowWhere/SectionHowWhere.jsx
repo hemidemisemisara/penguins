cadcd6bbd52d02ad09cc3f362f619cbdefb1e06a
Sara Chang <sara@Avocado.lan>
2024-06-06 19:04:47 -0700

    route the how & where section's edit button go to the how & where edit page

--- src/components/SectionHowWhere/SectionHowWhere.jsx
+++ src/components/SectionHowWhere/SectionHowWhere.jsx
@@ -2,8 +2,13 @@ import "./SectionHowWhere.scss";
 import Heading from "../Heading/Heading";
 import Button from "../Button/Button";
 import PhotoCardSmall from "../PhotoCardSmall/PhotoCardSmall";
+import { useNavigate } from "react-router-dom";
 
 export default function SectionHowWhere({ friendshipDetails }) {
+  const navigate = useNavigate();
+  const clickEdit = () => {
+    navigate("/how-where-edit");
+  };
   return (
     <section className="how-where">
       <Heading heading="how & where we met" />
@@ -15,7 +20,7 @@ export default function SectionHowWhere({ friendshipDetails }) {
       <p className="how-where__text">
         {friendshipDetails["how-where"].description}
       </p>
-      <Button label="edit" />
+      <Button label="edit" onClick={clickEdit} />
     </section>
   );
 }
